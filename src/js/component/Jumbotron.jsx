4aaff8837a6d2f77841d62dd3a8ae399be0a7ad9
xfontainex <maralafontaine@gmail.com>
2023-01-17 21:52:56 +0000

    I completed the Navbar

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -4,12 +4,12 @@ const Jumbotron = () => {
   return (
     <div>
       <div class="jumbotron jumbotron-fluid mx-2">
-        <div class="jumbotron-container w-100 px-4 pb-5" style={{background: "lightcyan"}}> 
+        <div class="jumbotron-container w-100 px-4 pt-1 pb-5" style={{background: "lightcyan"}}> 
           <h1 class="display-4">A Warm Welcome!</h1>
           <h4 class="lead">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste nesciunt ut tempora atque, maiores porro fuga iure modi, suscipit saepe sapiente. Magni laudantium, expedita molestias ratione eaque.
           </h4>
-          <a href="#" class="btn btn-primary rounded-0">
+          <a href="#" class="btn btn-primary rounded-0 mt-2">
             Call to Action!
           </a>
         </div>
--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -2,60 +2,30 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="bg-dark ps-3 d-flex justify-content-end">
-      <nav className="navbar navbar-expand-lg ">
-        <div className="d-flex">
-         <div className="">
-            <a className="navbar text-white text-decoration-none" href="#">
-              Start Bootstrap
-            </a>
-            </div>
-
-          <div className="d-flex">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse justify-content-end"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a
-                    className="nav-link active text-secondary"
-                    aria-current="page"
-                    href="#"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-secondary" href="#">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item ">
-                  <a className="nav-link text-secondary" href="#">
-                    Services
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-secondary">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark  px-4">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">Start Bootstrap</a>
+    <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-light">Disabled</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   );
 };
 
